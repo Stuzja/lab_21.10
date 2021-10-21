dc198f463a6b32508d5f7ba7985ab064749cee45
Анна Шишкина <shis@sfedu.ru>
2021-10-21 08:57:30 +0300

    Add function SecondsInMinute

--- DateTime.pas
+++ DateTime.pas
@@ -1,3 +1,4 @@
 ﻿begin
   function IsLeapYear(y:integer):=((y mod 4=0) and (DateTime.Now.Year mod 100<>0)) or (DateTime.Now.Year mod 400=0);
+  function SecondsInMInute(sec:integer):= sec mod 60;
 end.

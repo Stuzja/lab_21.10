f6a7605eec57374d3de3a5f16ff5b3edc27ea25e
Анна Шишкина <shis@sfedu.ru>
2021-10-21 09:09:48 +0300

    Add function LaterInYear

--- DateTime.pas
+++ DateTime.pas
@@ -1,3 +1,14 @@
 ﻿begin
   function IsLeapYear(y:integer):=((y mod 4=0) and (DateTime.Now.Year mod 100<>0)) or (DateTime.Now.Year mod 400=0);
+  function LaterInYear(d1,m1,d2,m2:integer):=string;
+  begin
+    if m1<m2 then
+      Result+=d2+'.'+m2
+    else if m2<m1 then
+      Result+=d1+'.'+m1
+    else if d1<d2 then
+      Result+=d2+'.'+m2
+    else 
+      Result+=d1+'.'+m1;
+  end;
 end.
